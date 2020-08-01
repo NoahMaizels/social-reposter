d993d2011935a87f86c1fa9b16c446bef27a7053
noahniuwa <noahmaizels@hotmail.com>
2020-08-01 17:12:02 -0600

    ...

--- middleware/forwarding.js
+++ middleware/forwarding.js
@@ -1,12 +1,14 @@
 
 const targetChatIds = process.env.TARGET_CHAT_IDS.split(',')
-const bot = require('./getBot.js')
+const targetChatIds = process.env.REPOST_IDS.split(',')
+
+const bot = require('../getBot.js')
 
 module.exports = (req, res, next) => {
-  if (req.body.channel_post){
+  const channel_id = req.body.channel_post.chat.id
+  const message_id = req.body.channel_post.message_id
+  if (req.body.channel_post && REPOST_IDS.includes(channel_id)){
     targetChatIds.forEach(chat_id => {
-      const channel_id = req.body.channel_post.chat.id
-      const message_id = req.body.channel_post.message_id
       // Forward all messages sent to channel where bot is admin to all TARGET_CHAT_IDS
       try {
         bot.forwardMessage(chat_id, channel_id, message_id)
